fd4f5cc741222097439c18a424aec171baa14c52
Blake <blandersonw9@gmail.com>
2025-09-24 08:05:35 -0400

    .

---
+++ faith/faith/faithApp.swift
@@ -0,0 +1,17 @@
+//
+//  faithApp.swift
+//  faith
+//
+//  Created by Blake Anderson on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct faithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
